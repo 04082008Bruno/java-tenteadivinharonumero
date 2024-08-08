085f1c3d7e81b86ede93170291309d763b473354
04082008Bruno <161615256+04082008Bruno@users.noreply.github.com>
2024-08-08 11:10:55 -0300

    mds

---
+++ codigo.js
@@ -0,0 +1,15 @@
+//declaração de variáveis 
+
+let quantidade = 0;
+let total = 0;
+
+//função  
+function calculaAna(){
+//promp para entrada de dados
+
+quantidade = parseInt(prompt("digite a quantidade de vezes que a ana quase desistiu:"));
+
+total = total + quantidade;
+
+alert(total);
+}
